b3bc782b01a57c270439a45d20663e6b6ee518ec
Marilou Pichol <marilou.pichol@laposte.net>
2022-08-17 16:36:10 +0200

    rails-yelp-mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.create([{ name: 'La Cabane de Anan', address: 'Boulevard Jean Marius', category: 'belgian' },
+                   { name: 'La table de Mathieu', address: 'Rue Sainte Helene', category: 'french' },
+                   { name: 'Chez Julien', address: 'chemin de Traverse', category: 'japanese' },
+                   { name: 'La table de Nana', address: 'Rue Sainte Marthe', category: 'belgian' },
+                   { name: 'La table de Titi', address: 'Rue Sainte', category: 'french' }])
